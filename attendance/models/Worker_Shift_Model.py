254e891acfee553043a49734a5d74f70ca36db86
Forhath Hosain Ome <62540867+Forhath-Hosain-Ome@users.noreply.github.com>
2025-07-20 09:31:12 +0000

    Worker Shift done

---
+++ attendance/models/Worker_Shift_Model.py
@@ -0,0 +1,9 @@
+from django.db import models
+from workers.models import WorkerModel
+from .Shift_Model import ShiftModel
+
+class WorkerShiftModel(models.Model):
+    worker = models.ForeignKey(WorkerModel, on_delete=models.CASCADE)
+    shift = models.OneToOneField(ShiftModel, on_delete=models.CASCADE)
+    assignment_date = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
