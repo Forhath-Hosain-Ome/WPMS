1ac6d43491a77a2eff270051de4c540c5af1956a
Forhath Hosain Ome <62540867+Forhath-Hosain-Ome@users.noreply.github.com>
2025-07-21 10:56:54 +0000

    models updated and admin panel updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import UserModel
 # Register your models here.
 
-admin.site.register(UserModel)
+@admin.register(UserModel)
+class UserModel(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'linked_worker')
+    search_fields = ('username', 'email')
+    list_filter = ('role',)
+    list_per_page = 25
+    search_help_text = "Use this field to search by keyword"
